9e409eb97e64ed1acd96a494973680b563a0b8ba
ICsharp1 <91024446+ICsharp1@users.noreply.github.com>
2025-02-18 22:13:47 +0200

    Delete lib directory

--- lib/constants/app_constants.dart
+++
@@ -1,9 +0,0 @@
-class AppConstants {
-  // Default notification time (10:00)
-  static const int DEFAULT_NOTIFICATION_HOUR = 10;
-  static const int DEFAULT_NOTIFICATION_MINUTE = 0;
-  
-  // Padding/Indentation values
-  static const double STEP_ITEM_PADDING = 8.0;
-  static const double STEP_CONTENT_PADDING = 12.0;
-}
